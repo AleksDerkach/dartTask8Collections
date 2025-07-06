557219bcca2b52faba007a1a9ac5fe570f63912b
Sasha Derkach <dosprg@gmail.com>
2025-07-06 16:28:03 +0300

    feat: task work with List.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,36 @@
+import 'dart:math';
+
 void main() {
-  
+   taskList(); 
+}
+
+taskList(){
+    final numbers = <int>[];
+    while (numbers.length < 100) {
+        numbers.add(Random().nextInt(101));
+    }
+    print(numbers);
+    print(numbers[64]);
+    numbers[49] = 1000000000;
+    numbers.removeAt(23);
+    numbers.removeAt(44);
+    numbers.removeAt(65);
+    numbers.removeAt(87);
+
+    int sum=0;
+    for (int i=0; i<numbers.length-1; i++) {
+      if(numbers[i]%3 == 0) {
+        sum += numbers[i]; 
+      } 
+    }
+    print('$sum - сума всіх елементів, що діляться на 3 без залишку');
+    
+    final temp = <int>[];
+    for (var digit in numbers) {
+        if(digit%2 == 0) {
+        temp.add(digit); 
+      }
+    }
+    print(temp);
+    print('${temp.length} - довжина списку temp');
 }
